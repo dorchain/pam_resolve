26a8ea44142606bc366cd8d7ecabbb66eae91600
Joerg Dorchain <joerg@dorchain.net>
2019-03-21 15:14:29 +0100

    handle PAM_SILENT

--- pam_resolve.c
+++ pam_resolve.c
@@ -33,9 +33,13 @@ struct addrinfo hints;
 char host[NI_MAXHOST];
 char *ap = NULL, *rap = NULL;
 int alen;
+int silent;
+int debug;
 
+silent = flags & PAM_SILENT;
+debug = !silent;
 if ((retval = pam_get_item (pamh, PAM_RHOST, (const void **) &rhost)) != PAM_SUCCESS) {
-	pam_syslog (pamh, LOG_ERR, "get rhost failed: %s", pam_strerror (pamh, retval));
+	if (!silent) pam_syslog (pamh, LOG_ERR, "get rhost failed: %s", pam_strerror (pamh, retval));
 	return retval;
 }
 
@@ -44,12 +48,12 @@ hints.ai_flags = AI_NUMERICHOST;
 hints.ai_family = AF_UNSPEC;
 hints.ai_socktype = SOCK_STREAM; /* we only want one result back */
 if ((retval = getaddrinfo(rhost, NULL, &hints, &res))) {
-	pam_syslog (pamh, LOG_DEBUG, "rhost %s is not a numeric IP: %s", (rhost == NULL) ? "<NULL>" : rhost, gai_strerror(retval));
+	if (debug) pam_syslog (pamh, LOG_DEBUG, "rhost %s is not a numeric IP: %s", (rhost == NULL) ? "<NULL>" : rhost, gai_strerror(retval));
 	return PAM_SUCCESS;
 }
 /* res should only have one entry, namely the given address */
 if (res->ai_next != NULL) {
-	pam_syslog (pamh, LOG_ERR, "%s is more than one entry", rhost);
+	if (!silent) pam_syslog (pamh, LOG_ERR, "%s is more than one entry", rhost);
 	freeaddrinfo(res);
 	return PAM_SUCCESS;
 }
@@ -57,7 +61,7 @@ if (res->ai_next != NULL) {
 retval = getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, 0, NI_NAMEREQD);
 
 if (retval) {
-	pam_syslog (pamh, LOG_DEBUG, "address %s does not resolve: %s", rhost, gai_strerror(retval));
+	if (debug) pam_syslog (pamh, LOG_DEBUG, "address %s does not resolve: %s", rhost, gai_strerror(retval));
 	freeaddrinfo(res);
 	return PAM_SUCCESS;
 }
@@ -68,7 +72,7 @@ hints.ai_family = res->ai_addr->sa_family;
 hints.ai_socktype = SOCK_STREAM;
 retval = getaddrinfo(host, NULL, &hints, &res0);
 if (!retval) {
-	pam_syslog (pamh, LOG_DEBUG, "address %s resolves to non-FDQN %s", rhost, host);
+	if (debug) pam_syslog (pamh, LOG_DEBUG, "address %s resolves to non-FDQN %s", rhost, host);
 	freeaddrinfo(res0);
 	freeaddrinfo(res);
 	return PAM_SUCCESS;
@@ -80,13 +84,13 @@ hints.ai_family = res->ai_addr->sa_family;
 hints.ai_socktype = SOCK_STREAM;
 retval = getaddrinfo(host, NULL, &hints, &res0);
 if (retval) {
-	pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s does not resolve: %s", rhost, host, gai_strerror(retval));
+	if (debug) pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s does not resolve: %s", rhost, host, gai_strerror(retval));
 	freeaddrinfo(res);
 	return PAM_SUCCESS;
 }
 if ((res0->ai_canonname == NULL || strcasecmp(host, res0->ai_canonname)) 
 	&& strcasecmp(host, "localhost")) {
-	pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s does not match %s",
+	if (debug) pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s does not match %s",
 		rhost, host, (res0->ai_canonname == NULL)? "<NULL>" : res0->ai_canonname);
 	freeaddrinfo(res0);
 	freeaddrinfo(res);
@@ -104,7 +108,7 @@ case AF_INET6:
 	alen = sizeof(struct in6_addr);
 	break;
 default:
-	pam_syslog (pamh, LOG_ERR, "unknown address family found for %s: %d", rhost, res->ai_family);
+	pam_syslog (pamh, LOG_CRIT, "unknown address family found for %s: %d", rhost, res->ai_family);
 	freeaddrinfo(res0);
 	freeaddrinfo(res);
 	return PAM_SUCCESS;
@@ -123,14 +127,14 @@ if (rp->ai_family == res->ai_family) {
 		}
 		break;
 	default:
-		pam_syslog (pamh, LOG_ERR, "unknown address family found for %s, %d", host, rp->ai_family);
+		pam_syslog (pamh, LOG_CRIT, "unknown address family found for %s, %d", host, rp->ai_family);
 		freeaddrinfo(res0);
 		freeaddrinfo(res);
 		return PAM_SUCCESS;
 		break;
 	}
 	if (memcmp(rap, ap, alen) == 0) { /* found it */
-		pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s, address matches,  keep it", rhost, host);
+		if (debug) pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s, address matches,  keep it", rhost, host);
 		freeaddrinfo(res0);
 		freeaddrinfo(res);
 		/* Good exit */
@@ -139,7 +143,7 @@ if (rp->ai_family == res->ai_family) {
 }
 }
 /* failure case */
-pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s, no address match", rhost, host);
+if (debug) pam_syslog (pamh, LOG_DEBUG, "%s resolves to %s, no address match", rhost, host);
 freeaddrinfo(res0);
 freeaddrinfo(res);
 return PAM_SUCCESS;
